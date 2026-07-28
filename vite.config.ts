fc0e589e9592859cb09042f819cbe5ac191b4cb2
Huawei Chen <huawei.chen@health.qld.gov.au>
2026-07-28 15:25:17 +1000

    add Squiz asset url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {
+    defineConfig
+} from 'vite'
+import react
+    from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    experimental: {
+        renderBuiltUrl(filename) {
+            return `?a=904377:dist/${filename}`
+        },
+    },
 })
